0ad6d7c6ed5fc24c91544b7ab228508e09989962
Odubo Timidi James <81032342+timidij@users.noreply.github.com>
2024-03-16 18:29:24 +0100

    using the if-else to check the grade

--- java.js
+++ java.js
@@ -7,7 +7,36 @@ function freeky(e){
     const test1 = document.querySelector("#test1").value
     const test2 = document.querySelector("#test2").value
     const exam = document.querySelector("#exam").value
+ let totalscore = parseInt(test1)+parseInt(test2)+parseInt(exam)
+    let grade = ""
+    if (totalscore >69){
+        grade ="A"
 
+    }
+    else if(totalscore >59){
+        grade ="B"
+    }
+    else if(totalscore >49){
+        grade="C"
+    }
+    else if (totalscore > 44){
+        grade = "D"
+
+    }else if (totalscore >39){
+        grade ="E"
+    }
+    else{
+        grade ="F"
+    }
+    //logic for message to be displayed
+    mess = ""
+ 
+    const str = `HELLO! ${fname} ${lname} with ${matno} your total score is ${totalscore}, your grade is ${grade} 
+    ${mess}
+    `
+    console.log(fname.value)
+    
+    header.innerHTML = str
    
 
 }
