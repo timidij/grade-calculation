4991f2ad7d76eda59c281563f874ea698d33c8ec
Odubo Timidi James <81032342+timidij@users.noreply.github.com>
2024-03-16 18:27:04 +0100

    query of tags in the html

--- java.js
+++ java.js
@@ -1,60 +1,19 @@
-
-// document.getElementById("namform")
-
-
-
-const hi = document.getElementById("dis")
-
-// document.addEventListener("submit",freeky)
-
-
-
-function freeky (e){
-    e.preventDefault()
- 
-   let fName= document.querySelector("#firstName")
-    let lName = document.querySelector("#lastName")
-    const matNo = document.querySelector("#matNo")
+  const header = document.querySelector("#dis")
+function freeky(e){
+    console.log(e)
+    const fname = document.querySelector("#firstName").value
+    const lname =  document.querySelector("#lastName").value
+    const matno = document.querySelector("#matNo").value
     const test1 = document.querySelector("#test1").value
-    const test2 = document.querySelector("#test2")
-    const exam = document.querySelector("#exam")
-    let grade = ''
-    let totalscore = parseInt(test1) + parseInt(test2.value) + parseInt(exam.value)
+    const test2 = document.querySelector("#test2").value
+    const exam = document.querySelector("#exam").value
 
-    if(totalscore >=70){
-        grade = "A"
-    }else if (totalscore > 60){
-        grade= "B"
-    }else if(totalscore>=50) {
-        grade = "C"
-    }else if(totalscore>= 45){
-        grade = "D"
+   
 
-    } else if(totalscore>=40){
-        grade = "E"
-    }
-    else{
-        grade = "F"
-    }
-    let congratulation = ""
-    if (totalscore >45){
-        congratulation = "CONGRATULATIONS you have passed "
-    }
-    else{
-        congratulation  = "UNFORTUNATELY you have failed"
-    }
-
-
-
-    // console.log(fName.value)
-    hi.innerHTML=`HELLO! ${fName.value} ${lName.value} with ${matNo.value}
-    your total score is ${totalscore}, your grade is ${grade} ${congratulation}
-    `
-    
 }
+document.addEventListener("DOMContentLoaded",()=>{
 
 
-// const btn = document.querySelector("#sub")
-// btn.addEventListener("click",freeky)
-const btn2 = document.querySelector("#nameform")
-btn2.addEventListener("submit", freeky)
+    const sub = document.querySelector("#nameform")
+    sub.addEventListener("submit", freeky)
+})
